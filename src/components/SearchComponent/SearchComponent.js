baad4d23d1ba58641db02260587dd915f0cda961
Mustafa Şahin <63456077+mtfsahin@users.noreply.github.com>
2023-04-26 17:48:30 +0300

    create: SearchComponent

--- src/components/SearchComponent/SearchComponent.js
+++ src/components/SearchComponent/SearchComponent.js
@@ -12,16 +12,9 @@ const SearchBox = (props) => {
     return (
         <div>
             {/* input box */}
-            <div className="font-sans">Search Box Helvetica G</div>
-            <div className="font-sans font-bold">Search Box Helvetica Bold</div>
-            <div className="font-starjedi">Search Box G</div>
-            <div className="font-starjhol">Search Box G</div>
-            <div>Search Box G</div>
-
-
             <input
                 type="text"
-                className="w-full py-2 pl-10 pr-3 leading-tight text-gray-700 bg-gray-200 border-2 border-transparent rounded-lg focus:outline-none focus:bg-white focus:border-gray-300"
+                className="w-full mt-3 py-2 pl-5 pr-3 text-primary-50 leading-tight bg-primary-950 hover:bg-primary-900 border-2 focus:p-2  transition-all duration-300 border-secondary-500 rounded-lg focus:outline-none focus:bg-primary-800 focus:border-secondary-300"
                 placeholder={props.placeholder}
                 value={query}
                 onChange={handleQueryChange}
@@ -29,5 +22,5 @@ const SearchBox = (props) => {
         </div>
     );
 };
-    
+
 export default SearchBox;
