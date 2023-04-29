25950853c04c2278dc8eb0cd308da9bf2e33677d
Mustafa Şahin <63456077+mtfsahin@users.noreply.github.com>
2023-04-29 23:24:17 +0300

    edit: add proporties

--- src/components/CardComponent/CardComponent.js
+++ src/components/CardComponent/CardComponent.js
@@ -3,9 +3,9 @@ import RatingComponent from '../RatingComponent/RatingComponent';
 import StarshipImg from '../../assets/img/starship_1.png'
 
 
-const Card = ({ title, subtitle }) => {
+const Card = ({ title, subtitle, rating, passengers , length, cargo_capacity}) => {
     return (
-        <div class="w-60 mt-7 transition-all duration-100 rounded-3xl overflow-hidden bg-primary-950 
+        <div class="w-64 mt-1 transition-all duration-100 rounded-3xl overflow-hidden bg-primary-950 
                     cursor-pointer border-2 border-secondary-500 
                     shadow-[5px_5px_rgba(0,_98,_90,_0.4),_10px_10px_rgba(0,_98,_90,_0.3),_15px_15px_rgba(0,_98,_90,_0.2),_20px_20px_rgba(0,_98,_90,_0.1),_25px_25px_rgba(0,_98,_90,_0.05)] 
                     hover:scale-105">
@@ -18,25 +18,25 @@ const Card = ({ title, subtitle }) => {
                 <div className="px-6 py-3 bg-primary-950">
                     {/* Rating Area */}
                     <div className='flex justify-center'>
-                        <RatingComponent rating={3} />
+                        <RatingComponent rating={Math.floor(rating)} />
                     </div>
                     {/* Functional area  */}
-                    <div className="flex  justify-center text-primary-100 text-md font-bold ">{title}</div>
+                    <div className="text-center overflow-hidden truncate text-primary-100 text-md font-bold">{title}</div>
                     {/* Functional area  */}
-                    <p className="flex justify-center text-primary-100  text-sm">{subtitle}</p>
+                    <p className="text-center overflow-hidden truncate text-primary-100 text-sm">{subtitle}</p>
                     {/* Functional area */}
                     <div className="text-primary-50 text-xs font-sans mt-2 ">
                         <div className="flex justify-between">
                             <p className="font-bold">Passengers:</p>
-                            <p className="ml-4 font-normal no-underline">Ship name</p>
+                            <p className="ml-4 font-normal no-underline">{(passengers)}</p>
                         </div>
                         <div className="flex justify-between mt-2">
-                            <p className="font-bold">Lenght:</p>
-                            <p className="ml-4 font-normal">100.000.0000</p>
+                            <p className="font-bold">Length:</p>
+                            <p className="ml-4 font-normal">{length}</p>
                         </div>
                         <div className="flex justify-between mt-2">
-                            <p className="font-bold">MLTG:</p>
-                            <p className="ml-4 font-normal">10000</p>
+                            <p className="font-bold">Cargo capacity:</p>
+                            <p className="ml-4 font-normal">{cargo_capacity}</p>
                         </div>
                     </div>
                 </div>
